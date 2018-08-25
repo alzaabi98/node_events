552f9c6e2c28d651309e3f4af216df8f44ebaab6
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2018-08-25 23:07:36 +0400

    first commit in lesson 2

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+
+
+app.get('/', (req,res)=> {
+
+    res.send(" it is working from web")
+})
+
+// bringg events routes
+
+const events = require('./routes/event-routes')
+app.use('/events', events)
+
+// listen to port 3000
+
+app.listen(3000, ()=> {
+
+    console.log(' app is wokring on port 3000')
+})
